c89372af0c21c3b4f00373961489da27832db040
infopkrajput <p.k.rajput@hotmail.com>
2025-04-29 00:03:00 +0530

    add all files

--- 07-Bash-Scripting/chapter-02-variables/userinfo.sh
+++ 07-Bash-Scripting/chapter-02-variables/userinfo.sh
@@ -1,4 +1,8 @@
-read "Enter your name: " name
-read "Enter your age: " age
-read "Enter the City: " city
-echo "Hello $name, you are $age years old and live in $city."
+#!/bin/bash
+echo "Enter your name: "
+read yourName
+echo "Enter your age: "
+read yourAge
+echo "Enter the City: "
+read yourCity
+echo "Hello $yourName, you are $yourAge years old and live in $yourCity."
